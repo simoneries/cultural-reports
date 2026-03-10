700a1e264c91be19d3a3daa30f36034e5c578d8f
simoneries <192712243+simoneries@users.noreply.github.com>
2026-03-10 20:22:25 +0100

    Retrieve data from paris musées in a list

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ from playwright.sync_api import sync_playwright
 
 
 #1 - Récuperer les principales expos à Paris dans une liste à print
+expos_louvre = []
+urlPM = "https://www.parismusees.paris.fr/fr/expositions"
+
 #1 - A - Récupérer les données du Louvre
+
+def get_paris_musees(url):
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(url)
+        name = page.locator(".view-content .title").all_inner_texts()
+        print(name)
+
+        browser.close()
+
+get_paris_musees(urlPM)
+
+
+
 #1 - B - Récupérer les données de plusieurs musées
 # 2 - Set up les données dans un DataFrame
