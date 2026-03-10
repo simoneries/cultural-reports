e3ecbf214c561c6377c15d3cb97c130aa6267a70
simoneries <192712243+simoneries@users.noreply.github.com>
2026-03-10 19:50:00 +0100

    Set up the initial plan

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Application visant à récupérer les principales expos en cours à Paris toutes les semaines
+
+"""
+from playwright.sync_api import sync_playwright
+
+
+#1 - Récuperer les principales expos à Paris dans une liste à print
+#1 - A - Récupérer les données du Louvre
+#1 - B - Récupérer les données de plusieurs musées
+# 2 - Set up les données dans un DataFrame
