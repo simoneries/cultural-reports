5efa2b71dfae2f0edd897fff492f589eadb69c1d
simoneries <192712243+simoneries@users.noreply.github.com>
2026-03-11 15:54:32 +0100

    Add beginning and end dates
    Use strptime func to convert strings in dates

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Application visant à récupérer les principales expos en cours à Paris toutes
 
 """
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
+from datetime import datetime
 
 
 #1 - Récuperer les principales expos à Paris dans une liste à print
@@ -15,7 +16,7 @@ def get_paris_musees(url):
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
-        page.goto(url)
+        page.goto(url,timeout=0)
         count = page.locator(".view-content a").count()
 
         for i in range(count):
@@ -23,20 +24,33 @@ def get_paris_musees(url):
             try: 
                 name = item.get_attribute("title")
             except: 
-                name = "Nom inconnu"
+                name = None
             
             try:
                 url_expo = item.get_attribute("href")# Rajouter le reste de l'Url
             except:
-                url_expo = "url inconnue" 
+                url_expo = None
 
             try: 
                 musee = item.locator(".nom-musee").first.inner_text()
             except PlaywrightTimeoutError:
-                musee = "Musee inconnu"
-            
+                musee = None
 
-            print(name,urlPM,musee)
+            try:
+                date_debut_loc = item.locator(".date-debut-evenement").all_inner_texts()
+                date_debut_str = date_debut_loc[0].replace("\n","-").replace("/","-")
+                date_format = '%d-%m-%y'
+                date_debut = datetime.strptime(date_debut_str,date_format)
+                date_fin_loc = item.locator(".date-fin-evenement").all_inner_texts()
+                date_fin_str = date_fin_loc[0].replace("\n","-").replace("/","-")
+                date_format = '%d-%m-%y'
+                date_fin = datetime.strptime(date_fin_str,date_format)
+
+            except PlaywrightTimeoutError:
+                date_debut = None
+                date_fin = None
+
+            print(name, url_expo, musee, date_debut,date_fin)
             
             #Next step récupérer les jours, le musée 
             #Step - Passer les données dans un DataFrame
